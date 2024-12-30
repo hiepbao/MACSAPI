093fa8bd9db79ff7704291b42d6131a9fade24c4
hiepbao <cubi27395@gmail.com>
2024-12-30 11:49:38 +0700

    #

--- obj/Debug/net8.0/MACSAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/MACSAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MACSAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e447b275e92503e7a3eb8e431bd0a5a54df72d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b60a40167cba05db08b21b05c557b56b14eeda8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MACSAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MACSAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
